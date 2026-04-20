598f165f87f79717fc62c356ee0770a33124e609
devhabeeblateef <habeeblateef002@gmail.com>
2026-04-20 19:06:23 +0100

    fix: remove wierd env shii

---
+++ config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  MAP_STYLES: {
+    road: "https://api.maptiler.com/maps/streets/style.json?key=Cgs7eUKoah6PEkf0csoY",
+    satellite: "https://api.maptiler.com/maps/satellite/style.json?key=Cgs7eUKoah6PEkf0csoY"
+  }
+};
